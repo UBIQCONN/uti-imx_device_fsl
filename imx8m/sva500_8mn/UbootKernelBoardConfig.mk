a21176bbe2296255c4b11f2c4f24531a464fa5f6
ShihYuan-Huang <timmy@ubiqconn.com>
2024-01-05 15:21:58 +0800

    Fix sva500 uboot build error

--- imx8m/sva500_8mn/UbootKernelBoardConfig.mk
+++ imx8m/sva500_8mn/UbootKernelBoardConfig.mk
@@ -4,7 +4,7 @@ UBOOT_POST_PROCESS := true
 
 # u-boot target for stand config and Trusty OS config
 TARGET_BOOTLOADER_CONFIG := imx8mn-ddr4:imx8mn_ddr4_sva500_android_defconfig
-#TARGET_BOOTLOADER_CONFIG += imx8mn:imx8mn_evk_android_defconfig
+TARGET_BOOTLOADER_CONFIG += imx8mn:imx8mn_evk_android_defconfig
 TARGET_BOOTLOADER_CONFIG += imx8mn-dual:imx8mn_evk_android_dual_defconfig
 ifeq ($(PRODUCT_IMX_TRUSTY),true)
   TARGET_BOOTLOADER_CONFIG += imx8mn-trusty:imx8mn_evk_android_trusty_defconfig
@@ -14,7 +14,7 @@ endif
 
 # u-boot target used by uuu for imx8mn_evk
 TARGET_BOOTLOADER_CONFIG += imx8mn-ddr4-evk-uuu:imx8mn_ddr4_sva500_android_uuu_defconfig
-#TARGET_BOOTLOADER_CONFIG += imx8mn-evk-uuu:imx8mn_evk_android_uuu_defconfig
+TARGET_BOOTLOADER_CONFIG += imx8mn-evk-uuu:imx8mn_evk_android_uuu_defconfig
 
 ifeq ($(IMX8MN_USES_GKI),true)
 TARGET_KERNEL_DEFCONFIG := gki_defconfig
